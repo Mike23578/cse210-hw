e5d2bb642d56c7736fc8fe5b6dfa41ca3ca97ac6
Mike23578 <miguelsarabiaheredia@gmail.com>
2026-05-09 21:06:24 -0700

    Work on the ifs statements for each grade percentage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+Console.Write("Whats your grade percentage? ");
+string gradeInput = Console.ReadLine();
+double gradePercentage = double.Parse(gradeInput);
+
+if (gradePercentage < 70 || gradePercentage == 100)
 {
-    static void Main(string[] args)
+    if (gradePercentage >= 90)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Your letter grade is: A");
     }
+    else if (gradePercentage >= 80)
+    {
+        Console.WriteLine("Your letter grade is: B");
+    }
+    else if (gradePercentage >= 70)
+    {
+        Console.WriteLine("Your letter grade is: C");
+    }
+    else if (gradePercentage >= 60)
+    {
+        Console.WriteLine("Your letter grade is: D");
+    }
+    else
+    {
+        Console.WriteLine("Your letter grade is: F");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid grade percentage. Please enter a value between 0 and 100.");
 }
