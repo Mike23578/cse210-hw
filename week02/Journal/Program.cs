4e1254a3f6b245800019bdea55d9ec8d2e214eb5
Mike23578 <miguelsarabiaheredia@gmail.com>
2026-05-13 10:55:06 -0700

    Deleted the program from the wrong week

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,34 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your grade percentage? ");
-        string grade = Console.ReadLine();
-        int gradePercentage = int.Parse(grade);
-
-        if (gradePercentage >= 90)
-        {
-            Console.WriteLine("Your letter grade is an A.");
-        }
-        else if (gradePercentage >= 80)
-        {
-            Console.WriteLine("Your letter grade is a B.");
-        }
-        else if (gradePercentage >= 70)
-        {
-            Console.WriteLine("Your letter grade is a C.");
-        }
-        else if (gradePercentage >= 60)
-        {
-            Console.WriteLine("Your letter grade is a D.");
-        }
-        else if (gradePercentage < 60)
-        {
-            Console.WriteLine("Your letter grade is an F.");
-        }
-        else
-        {
-            Console.WriteLine("Invalid grade percentage.");
-        }
-         
+        
     }
 }
