6a0941029640d77470fb008ed49e8b446e1d7137
Mike23578 <miguelsarabiaheredia@gmail.com>
2026-05-13 22:02:26 -0700

    Finished W1 Activity 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int sum = number;
+        int count = 1;
+        int average = 0;
+        int largest = number;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            sum += number;
+            count++;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        average = sum / (count - 1);
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+        Console.WriteLine($"The average of the numbers is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
